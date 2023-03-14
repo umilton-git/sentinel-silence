cbad265a00bd0970c8d9655895c4d670fcfdff32
umilton-git <ulysses.github@protonmail.com>
2023-03-14 15:27:58 -0400

    Added some functionality to menu, hooked up stat info to pause screen assets

--- Assets/Scripts/Dialogue/DialogueActivator.cs
+++ Assets/Scripts/Dialogue/DialogueActivator.cs
@@ -6,6 +6,7 @@ public class DialogueActivator : MonoBehaviour
 {
     public string[] lines;
     private bool canActivate;
+    
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/gameMenu.cs
+++ Assets/Scripts/gameMenu.cs
@@ -1,16 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class gameMenu : MonoBehaviour
 {
     public GameObject menu;
     public static bool isPaused = false;
+    private CharStats[] playerStats;
+
+    public TMP_Text[] nameText, HpText, SpText, lvlText;
+    public Slider[] HpSlider, SpSlider;
+    public Image[] charImage;
+    public GameObject[] charStatHolder;
+    public Button[] menuButtons;
+    public Button Tracker;
+    private int trackerPos;
+    private ColorBlock buttonColor;
+    private ColorBlock buttonColorPressed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Tracker.transform.position = menuButtons[0].transform.position;
+        trackerPos = 0;
+        buttonColor = Tracker.colors;
+        buttonColorPressed.normalColor = new Color(0.0f, 0.0f, 0.0f, 1.0f);
     }
 
     // Update is called once per frame
@@ -21,15 +37,56 @@ public class gameMenu : MonoBehaviour
             if(menu.activeInHierarchy)
             {
                 menu.SetActive(false);
+                trackerPos = 0;
                 ResumeGame();
             }
             else
             {
                 menu.SetActive(true);
+                UpdateMainStats();
                 PauseGame();
             }
         }
-        
+
+        if(Input.GetKeyDown(KeyCode.DownArrow) && isPaused)
+        {
+            if(trackerPos < menuButtons.Length - 1)
+            {
+                trackerPos += 1;
+                Tracker.transform.position = menuButtons[trackerPos].transform.position;
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.UpArrow) && isPaused)
+        {
+            if(trackerPos > 0)
+            {
+                trackerPos -= 1;
+                Tracker.transform.position = menuButtons[trackerPos].transform.position;
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.Z) && isPaused)
+        {
+            Tracker.colors = buttonColorPressed;   
+        }
+
+        if(Input.GetKeyUp(KeyCode.Z) && isPaused)
+        {
+            Tracker.colors = buttonColor;
+            if(menuButtons[trackerPos].name == "Close")
+            {
+                menu.SetActive(false);
+                trackerPos = 0;
+                ResumeGame();
+            } 
+
+            if(menuButtons[trackerPos].name == "Quit")
+            {
+                trackerPos = 0;
+                Application.Quit();
+            } 
+        }
     }
 
     void PauseGame()
@@ -40,7 +97,33 @@ public class gameMenu : MonoBehaviour
 
     void ResumeGame()
     {
+        Tracker.transform.position = menuButtons[trackerPos].transform.position;
         Time.timeScale = 1;
         isPaused = false;
+    }
+
+    public void UpdateMainStats()
+    {
+        playerStats = GameManager.instance.playerStats;
+
+        for(int i = 0; i < playerStats.Length; i++)
+        {
+            if(playerStats[i].gameObject.activeInHierarchy)
+            {
+                charStatHolder[i].SetActive(true);
+                nameText[i].text = playerStats[i].charName;
+                HpText[i].text = "HP: " + playerStats[i].currentHP + "/" + playerStats[i].maxHP;
+                SpText[i].text = "SP: " + playerStats[i].currentSpirit + "/" + playerStats[i].maxSpirit;
+                lvlText[i].text = "Lv: " + playerStats[i].playerLevel;
+                HpSlider[i].maxValue = playerStats[i].maxHP;
+                HpSlider[i].value = playerStats[i].currentHP;
+                SpSlider[i].maxValue = playerStats[i].maxSpirit;
+                SpSlider[i].value = playerStats[i].currentSpirit;
+                charImage[i].sprite = playerStats[i].Char;
+            } else
+            {
+                charStatHolder[i].SetActive(false);
+            }
+        }
     }
 }
